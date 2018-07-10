30fb925e43a42d272734d504feceb58a911de2bc
Tatiana190799 <41038639+Tatiana190799@users.noreply.github.com>
2018-07-10 12:30:29 +0600

    refactoring calculator

--- Calculator/Calculator/Calculator/Properties/Resources.Designer.cs
+++ Calculator/Calculator/Calculator/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace calculato.Properties
+namespace Calculator.Properties
 {
 
 
@@ -44,7 +44,7 @@ namespace calculato.Properties
             {
                 if ((resourceMan == null))
                 {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("calculato.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Calculator.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- Calculator/Calculator/Calculator/Properties/Settings.Designer.cs
+++ Calculator/Calculator/Calculator/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace calculato.Properties
+namespace Calculator.Properties
 {
 
 
